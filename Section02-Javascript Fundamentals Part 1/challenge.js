ad022e6f2d5fcce3299e7d37b3386d2a9dfdad60
QUỲNH THI PHẠM <thipham0502@gmail.com>
2020-12-16 11:00:20 +0700

    Dec16_1100

--- Section02-Javascript Fundamentals Part 1/challenge.js
+++ Section02-Javascript Fundamentals Part 1/challenge.js
@@ -1,11 +1,18 @@
-/* Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
-BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter) */
+/* Challenge #1: Mark and John are trying to compare their BMI (Body Mass Index), which is 
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter)
 
-/* 1. Store Mark's and John's mass and height in variables
+1. Store Mark's and John's mass and height in variables
 2. Calculate both their BMIs using the formula (you can even implement both
 versions)
 3. Create a Boolean variable 'markHigherBMI' containing information about
 whether Mark has a higher BMI than John */
+
+/* Challenge #2: 
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!" */
 
 const mass_Mark = 75,
 	mass_John = 86,
@@ -16,8 +23,33 @@ const bmi_Mark = mass_Mark / height_Mark ** 2,
 const markHigherBMI = bmi_Mark > bmi_John;
 console.log('Mark:', mass_Mark, height_Mark, bmi_Mark);
 console.log('John:', mass_John, height_John, bmi_John);
-if (markHigherBMI) {
-	console.log("Mark's BMI is higher than John");
-} else {
-	console.log("Mark's BMI is lower than John");
-}
+if (markHigherBMI) console.log(`Mark's BMI: ${bmi_Mark.toFixed(2)} is higher than John's: ${bmi_John.toFixed(2)}!`);
+else console.log(`John's BMI: ${bmi_John.toFixed(2)} is higher than Mark's: ${bmi_Mark.toFixed(2)}!`);
+
+/* Challenge #3: 
+There are two gymnastics teams, Dolphins and Koalas. They compete against each 
+other 3 times. The winner with the highest average score wins a trophy!
+
+1. Calculate the average score for each team, using the test data below:
+Test data:
+Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks 😉
+
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy */
+const avgDolphins = (96 + 108 + 89) / 3,
+	avgKoalas = (88 + 91 + 110) / 3;
+if (avgDolphins > avgKoalas) console.log('Dolphins win!');
+else if (avgDolphins < avgKoalas) console.log('Koalas win!');
+else console.log('Draw!');
--- Section02-Javascript Fundamentals Part 1/script.js
+++ Section02-Javascript Fundamentals Part 1/script.js
@@ -20,3 +20,119 @@ console.log("You shouldn't declare variables without using let/const/var like th
 
 /* Lec15. Operator Precedence: mức độ ưu tiên toán tử */
 // https://developer.mozilla.org/vi/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#Table
+
+/* Lec17. Strings and Template Literals */
+const lastName = 'Michael';
+const job = 'Teacher';
+const currentYear = 2020;
+const myBirthYear = 1991;
+
+// Print this pattern: "I'm <lastName>, a (<currentYear> - <myBirthYear>) <job>!"
+const myIntroduction = `I'm ${firstName} ${lastName}, a ${currentYear - myBirthYear} years old ${job.toLowerCase()}!`;
+console.log('My introduction using string literal:', myIntroduction);
+
+//Multiline string
+console.log('This is\na multi\nline\nstring with \\n!');
+console.log('This is\n\
+a multi\n\
+line\n\
+string with \\n\\!');
+console.log(`This is
+a multi
+line
+string with backtick!`);
+
+/* Lec18. If/else */
+// Adding emoji ^^: Window and '.'
+console.log(`Some icon: 🎶💕✨`);
+
+/* Lec20. Type conversion and Coercion */
+// Conversion: manually convert
+// Coercion: Js automatically and implicitly convert
+
+/* string -> number */
+const strInputYear = '1998';
+console.log(strInputYear, typeof strInputYear);
+
+const numInputYear = Number(strInputYear);
+console.log(numInputYear, typeof Number(numInputYear));
+
+const _str = 'abc';
+console.log(Number(_str)); //NaN
+console.log('typeof NaN:', typeof NaN); //number, because NotANumber is an invalid number
+
+/* number to string */
+console.log('typeof String(10):', typeof String(10));
+
+/* Coercion */
+// '+' operator: string concat
+// other operator: convert the variable to number type to calculate
+console.log('I am ' + 22 + ' years old');
+console.log('30' - '20' + 5); //15
+console.log('30' + '20' + 5); //30205 (string concat)
+console.log('30' + '20' - 5); //3020 (string concat) - 5 = 3015
+console.log('30' * '5'); //150
+console.log('30' / 5); //6
+console.log('30' > '10'); //true
+console.log('30' > 10); //true
+
+let n = '1' + 2; //12 (string concat)
+console.log(typeof n); //string
+n = n - 1; //11
+console.log(typeof n); //number
+n = n + 1; //12
+console.log(n); //12
+
+const a = 10 + 5 + '3'; // -> (10+5) + '3" = 15 + '3' = 153 (string concat)
+console.log(a); //153
+
+/* Lec21. Truthy & Falsy values */
+// 5 falsy values: 0, '', undefined, null, NaN
+// All others are truthy values
+console.log(Boolean(0)); //false
+console.log(Boolean('')); //false
+console.log(Boolean(undefined)); //false
+console.log(Boolean(null)); //false
+console.log(Boolean(NaN)); //false
+
+console.log(Boolean(100)); //true
+console.log(Boolean('abc')); //true
+console.log(Boolean({})); //true {} is an empty object
+
+const money = 0;
+if (!money) console.log('No money');
+else console.log('You have money!');
+
+/* Lec22. Equality operators: == vs. === */
+// ==: loosely equal (type coercion, same value)
+// ===: strictly equal (same type, same value)
+console.log(18 === 18); //true
+console.log(18 === '18'); //false
+console.log(18 == '18'); //true
+
+// Input prompt
+// const str_year = prompt('Input your birth year:'); //enter string
+// const num_year = Number(prompt('Input your birth year:')); //convert enter string to number
+// document.getElementById('text1').innerHTML = `Your age is ${2020 - num_year}`;
+// console.log('str_year:', typeof str_year, str_year);
+// console.log('num_year:', typeof num_year, num_year);
+
+// != vs !==
+// !=: loosely unequal (type coercion any type, not same value)
+// !==: strictly unequal (not same type, not same value)
+console.log('\n');
+console.log(18 !== 20); //true
+console.log(18 !== 18); //false
+console.log(18 !== '18'); //true
+console.log(18 != 20); //true
+console.log(18 != '20'); //true
+console.log(18 != '18'); //false
+
+/* Lec24. Logical operators */
+const isMale = true;
+const isAdult = false;
+if (isMale && isAdult) console.log('Man');
+else if (isMale && !isAdult) console.log('Boy');
+else if (!isMale && isAdult) console.log('Woman');
+else if (!isMale && !isAdult) console.log('Girl');
+else console.log('Characteristic undefined!');
