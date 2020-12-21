64b1168b7d19c75cad53eff618c8db8b099c4119
QUỲNH THI PHẠM <thipham0502@gmail.com>
2020-12-21 17:57:10 +0700

    Dec21_1757

--- Section08-How Javascript Works Behind the Scenes/script8.js
+++ Section08-How Javascript Works Behind the Scenes/script8.js
@@ -1,1 +1,258 @@
 'use strict';
+
+/* Lec92. Scope and the Scope Chain */
+function calcAge(birthYear) {
+	const age = 2020 - birthYear; //function scoped
+
+	console.log(firstName); //accessible
+	//dont use 'name' for variable because 'name' is a special word in Js
+
+	function printAge() {
+		const output = `${firstName}, you are ${age}, born in ${birthYear}`; //firstName accessible from the global scope; age, birthYear accessible from the parent function
+		console.log(output);
+
+		if (age >= 18) {
+			var adult = true;
+
+			const firstName = 'Thi';
+			const str = `Oh, and you're an adult, ${firstName}`; //Thi, not Kathy, because it takes value at the current scope
+			console.log(str);
+
+			//If 'use strict' mode, then functions can also be block scoped (function inside a block cannot be accessed outside of the block)
+			function add(a, b) {
+				return a + b;
+			}
+
+			console.log(add(2, 3)); //accessible, function 'add' is block scoped
+		}
+		// console.log(str); //inaccesible, 'str' is block scoped
+
+		console.log(adult); //accessible, declared with 'var' -> ignore block scope (but still affected by function scope)
+
+		// add(2, 3); //inaccessible, function 'add' is block scoped (and we're using 'strict' mode)
+	}
+	printAge();
+	// console.log(adult); //inaccessible, 'adult' is function scoped
+
+	return age;
+}
+
+const firstName = 'Kathy'; //global scoped
+calcAge(1998);
+
+// console.log(age); //inaccessible (ReferenceError), 'age' is function scoped
+// printAge(); //inaccessible (ReferenceError), 'printAge()' is function scoped
+
+/* Lec95. TDZ (Temporal Dead Zone) (let & const) */
+console.log(me); //accessible (can be called the before it is defined)
+
+//console.log(job); //inaccessible (ReferenceError: Cannot access 'job' before initialization) -> We are calling 'job' is in its Temporal Dead Zone (cannot be called the before it is defined)
+
+//console.log(year); //inaccessible (ReferenceError: Cannot access 'year' before initialization) -> We are calling 'year' is in its Temporal Dead Zone (cannot be called the before it is defined)
+
+var me = 'Kathy'; //not affected by Temporal Dead Zone
+let job = 'Dev';
+const year = 1998;
+
+/* Temporal Dead Zone (functions) */
+console.log(addDeclaration(2, 3)); //accessible -> We can call the function declaration before it is defined
+
+/*
+console.log(addExpression_const(2, 3)); //inaccessible (ReferenceError: Cannot access 'addExpression_const' before initialization) -> We are calling 'addExpression' is in its Temporal Dead Zone (cannot call the before it is defined)
+
+console.log(addArrow_const(2, 3)); //inaccessible (ReferenceError: Cannot access 'addArrow_const' before initialization) -> We are calling 'addArrow' is in its Temporal Dead Zone (cannot call the before it is defined)
+
+console.log(addExpression_var(2, 3)); //inaccessible (TypeError: 'addExpression_var' is not a function) -> addExpression_var is 'undefined'
+
+console.log(addArrow_var(2, 3)); //inaccessible (TypeError: 'addArrow_var' is not a function) -> addExpression_var is 'undefined'
+*/
+// Declaration style
+function addDeclaration(a, b) {
+	return a + b;
+}
+
+// Expression style with const
+const addExpression_const = function(a, b) {
+	return a + b;
+};
+
+// Expression style with var
+var addExpression_var = function(a, b) {
+	return a + b;
+};
+
+// Arrow style with const
+const addArrow_const = (a, b) => a + b;
+
+// Arrow style with var
+var addArrow_var = (a, b) => a + b;
+
+/* Global window object */
+var x = 10;
+let y = 11;
+const z = 12;
+
+console.log(window); //window object
+console.log(x === window.x); //true: x becomes a property of the window object (declared with 'var')
+console.log(y === window.y); //false: y is NOT a property of the window object (declared with 'let')
+console.log(z === window.z); //false: z is NOT a property of the window object (declared with 'const')
+
+console.log(addExpression_var === window.addExpression_var); //true: addExpression_var becomes a property of the window object (declared with 'var')
+console.log(addExpression_var === window.addExpression_var); //true: addExpression_var becomes a property of the window object (declared with 'var')
+
+/* Lec96. 'this' keyword */
+// Its value is only assigned when the function is actually called
+// For Arrow functions, 'this' will be of the surrounding (parent) function
+// For Event listener, 'this' is the DOM element of the handler
+
+console.log(this); //'this' in the global scope is the global window object
+
+// Regular function call in 'strict' mode: 'this' === undefined
+const calcAge_Regular = function(birthYear) {
+	console.log(2020 - birthYear);
+	console.log(this); //'this' === undefined
+};
+calcAge_Regular(1998); //undefined
+
+// Arrow function: 'this' get the 'this' of the parent's function -> 'this' in the global scope -> the global window object
+const calcAge_Arrow = (birthYear) => {
+	console.log(2020 - birthYear);
+	console.log(this); //'this' === window object
+};
+calcAge_Arrow(1998); //undefined
+
+// Function is a method of an object -> 'this' === the object calling it
+const kathy = {
+	myName: 'Kathy',
+	year: 1998,
+	calcAge_Object: function() {
+		console.log(this); //'this' === the object calling it
+		console.log(`${this.myName} is ${2020 - this.year} years old.`);
+	}
+};
+kathy.calcAge_Object(); //'Kathy is 22 years old.'
+
+// Copy method of object A to object B
+const sean = {
+	myName: 'Sean',
+	year: 1991
+};
+sean.calcAge = kathy.calcAge_Object; //copy
+sean.calcAge(); //'Sean is 29 years old.'
+
+const func = kathy.calcAge_Object;
+//func(); //cannot access 'this.myName', 'this.year' because no object is calling 'func' ('this' === undefined) / same as the regular function call
+
+/* Lec98. Regular Functions vs. Arrow Functions */
+const john = {
+	lastName: 'Michael',
+	year: 1997,
+	calcAge1: function() {
+		//console.log(this); //'this' === the object calling it
+		console.log(`${this.lastName} is ${2020 - this.year} years old.`);
+
+		// Regular function inside method: 'this' still === undefined
+		const isAdult = function() {
+			console.log(this); //undefined
+			console.log(2020 - this.year >= 18 ? `${this.lastName} is an adult` : `${this.lastName} is not an adult`); //cannot access 'this.year', 'this.lastName'
+		};
+		isAdult(); //Regular function call (no object is calling it -> this === undefined)
+	},
+	calcAge2: function() {
+		const self = this; //self (that) = this = object 'john' (save/preserve the 'this' value so that we can use it in the regular function 'isAdult')
+
+		// Regular function inside method: 'this' still === undefined
+		const isAdult = function() {
+			//console.log(this); //undefined
+			console.log(self); //object 'john'
+			console.log(2020 - self.year >= 18 ? `${self.lastName} is an adult` : `${self.lastName} is not an adult`); //'self.year' = 1997, 'self.lastName' = 'Michael'
+		};
+		isAdult(); //No error
+	},
+	calcAge3: function() {
+		// Arrow function inside method: 'this' from the parent scope === 'john' object
+		const isAdult = () => {
+			console.log(this); //'john' object
+			console.log(2020 - this.year >= 18 ? `${this.lastName} is an adult` : `${this.lastName} is not an adult`); //'this.year' = 1997, 'this.lastName' = 'Michael'
+		};
+		isAdult(); //No error
+	},
+
+	//Arrow function: 'this' get the 'this' of the parent's function -> 'this' is the global window object -> window object has no variable lastName -> undefined (NOTE: Object is not a scope)
+	greet: () => console.log(`Hey ${this.lastName}!`) //this.lastName === undefined
+};
+john.greet(); //'Hey undefined!'
+console.log(this.lastName); //window.lastName === undefined
+
+// Define a variable with 'var' -> window.lastName can be called
+var lastName = 'Park'; //create a property on the global window object
+john.greet(); //Hey Park!
+console.log(this);
+
+//john.calcAge1(); //TypeError: Cannot read property 'year' of undefined (error at the 'isAdult' function)
+
+john.calcAge2(); //No error
+
+/* 'arguments' keyword */
+// Using 'arguments' keyword in regular function
+function addDeclaration2(a, b) {
+	console.log(arguments);
+	return a + b;
+}
+addDeclaration2(1, 2); //[1, 2]
+
+// Using 'arguments' keyword in expression function
+var addExpression = function(a, b) {
+	console.log(arguments);
+	return a + b;
+};
+addExpression(2, 3); //[2, 3]
+addExpression(2, 3, 4, 5); //[2, 3, 4, 5] (even though the function only has 2 parameters)
+//=> We can loop over this list of arguments (parameters) to do calculations
+
+// Using 'arguments' keyword in arrow function -> Error!
+var addArrow = (a, b) => {
+	console.log(arguments);
+	return a + b;
+};
+//addArrow(2, 3); //ReferenceError: arguments is not defined
+
+/* Lec99. Primitives vs. Objects (Primitive vs. Reference Types) */
+// Primitives: numbers, booleans, strings, Undefined, Null, Symbol, BigInt -> Primitive Types
+// Objects (anything else): Object, Arrays, Functions, etc. -> Reference Types
+
+// Primitive values are stored in Call Stack (storing its address and value)
+// Reference values are stored in Heap (Call Stack will store its address to the heap)
+let age = 30;
+let oldAge = age; //oldAge = age = 30
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const myself = {
+	name: 'Kathy',
+	age: 22
+};
+const myfriend = myself; //address of 'myself' = address of 'myfriend' (2 variables but point to only 1 object ~ 1 address in the memory heap)
+myfriend.name = 'John'; //also change 'name' of 'myself'
+console.log('Friend:', myfriend); //name = 'John'
+console.log('Me:', myself); //name = 'John'
+
+//Copying objects
+const myselfCopy1 = Object.assign({}, myself); //Copy from source object(s) to a target object -> Create new object with same properties
+myselfCopy1.name = 'Mary';
+console.log(myselfCopy1); //name = 'Mary'
+
+// NOTE: Object.assign is just a shallow copy ie. just copying properties in the first level, cannot copy its child/nested reference values (still point to the same memory address)
+const myself2 = {
+	name: 'Kathy',
+	age: 22,
+	family: [ 'Alice', 'Bob' ] //array is an reference value
+};
+const myselfCopy2 = Object.assign({}, myself2);
+
+myselfCopy2.family.push('Mary'); //also change 'family' of 'myself2'
+console.log('myself2:', myself2); //["Alice", "Bob", "Mary"]
+console.log('myselfCopy2:', myself2); //["Alice", "Bob", "Mary"]
+
+// NOTE: Can use library Lo-Dash to make deep cloning (copying nested objects)
---
+++ Section09-Data Structure, Modern Operators and Strings/script9.js
@@ -0,0 +1,223 @@
+'use script';
+
+// -----Modern Javascript focus----- \\
+
+/* Lec103. Destructuring arrays */
+// Unpacking values in array
+const arr = [ 'Kathy', 22, 'red' ];
+const [ firstName, age, color ] = arr;
+console.log(firstName, age, color);
+
+// Unpacking values in array inside an object
+let restaurant = {
+	name: 'Classico Italiano',
+	location: 'Via Angelo Tavanti 23, Firenze, Italy',
+	categories: [ 'Italian', 'Pizzeria', 'Vegetarian', 'Organic' ],
+	starterMenu: [ 'Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad' ],
+	mainMenu: [ 'Pizza', 'Pasta', 'Risotto' ],
+
+	//return the starter and main dish according to their index
+	order: function(starterIndex, mainIndex) {
+		return [ this.starterMenu[starterIndex], this.mainMenu[mainIndex] ];
+	}
+};
+
+// Take out the 1st and 2nd category in 'restaurant' object
+let [ firstCategory, secondCategory ] = restaurant.categories;
+console.log(firstCategory, secondCategory); //Italian, Pizzeria
+
+// Take out the 1st and 3rd category in 'restaurant' object
+[ firstCategory, , thirdCategory ] = restaurant.categories;
+console.log(firstCategory, thirdCategory); //Italian, Vegetarian
+
+// Changing values: Swap 1st and 2nd category
+//// Solution 1:
+// const temp = firstCategory;
+// firstCategory = secondCategory;
+// secondCategory = temp;
+// console.log(firstCategory, secondCategory); //Pizzeria, Italian
+
+//// Solution 2:
+[ firstCategory, secondCategory ] = [ secondCategory, firstCategory ];
+console.log(firstCategory, secondCategory); //Pizzeria, Italian
+
+// Destructuring arrays returned from a function
+const [ starterDish, mainDish ] = restaurant.order(1, 0); //take starter dish with index = 1 and main dish with index = 0
+console.log(starterDish, mainDish); //Bruschetta, Pizza
+
+// Destructuring nested arrays
+const nested = [ 2, 4, [ 5, 6 ] ];
+let [ a, b, c ] = nested;
+console.log(a, c); //2, [5, 6]
+
+let [ , , [ c1, c2 ] ] = nested;
+console.log(c1, c2); //5, 6
+
+[ a, b, [ c1, c2 ] ] = nested;
+console.log(a, b, c1, c2); //2, 4, 5, 6
+
+// Destructuring arrays with default values (used when we dont know exactly how many elements in the array)
+let [ x, y, z ] = [ 0, 9 ];
+console.log(x, y, z); //0, 9, undefined
+
+[ x = 1, y = 1, z = 1 ] = [ 8, 9 ]; //default = 1 for undefined values
+console.log(x, y, z); //8, 9, 1
+
+/* Lec103. Destructuring objects */
+restaurant = {
+	name: 'Classico Italiano',
+	location: 'Via Angelo Tavanti 23, Firenze, Italy',
+	categories: [ 'Italian', 'Pizzeria', 'Vegetarian', 'Organic' ],
+	starterMenu: [ 'Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad' ],
+	mainMenu: [ 'Pizza', 'Pasta', 'Risotto' ],
+
+	openingHours: {
+		thu: {
+			open: 12,
+			close: 22
+		},
+		fri: {
+			open: 11,
+			close: 23
+		},
+		sat: {
+			open: 0, // Open 24 hours
+			close: 24
+		}
+	},
+
+	//return the starter and main dish according to their index
+	order: function(starterIndex, mainIndex) {
+		return [ this.starterMenu[starterIndex], this.mainMenu[mainIndex] ];
+	},
+
+	//destructuring object when parsing to function, with default values
+	orderDelivery: function({ starterIndex = 0, mainIndex = 0, time = '08:03', address }) {
+		console.log(
+			`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[
+				mainIndex
+			]} will be delivered to ${address} at ${time}`
+		);
+	}
+};
+
+// Take out the 'name', 'openingHours' and 'categories' in 'restaurant' object
+// NOTE: order of properties is not important, but must provide the exact property name
+let { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// What if we can to change the property names?
+const { name: restaurantName, openingHours: hours, categories: tags } = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Destructuring objects with default values (used when we dont know exactly the property names)
+let { menu = [], starterMenu: starters = [] } = restaurant; //with default
+console.log(menu); //menu = []
+console.log(starters); //starters = ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"]
+
+let { menu2, starterMenu = [] } = restaurant; //without default
+console.log(menu2); //menu2 = undefined
+
+// Reassign variables with object's values
+let m = 11;
+let n = 99;
+console.log(m, n); //11, 99
+const obj = { m: 23, n: 7, o: 14 };
+
+// m, n already be defined, but now we want to reassign (overwrite) it with m, n values from 'obj'
+({ m, n } = obj); //put the expression inside ()
+console.log(m, n); //23, 7
+
+// Destructuring nested objects
+// Take out the opening and closing hour on Friday
+const { fri: { open, close } } = openingHours;
+console.log(open, close); //11, 23
+
+// Change the property name
+const { fri: { open: openAt, close: closeAt } } = openingHours;
+console.log(openAt, closeAt); //11, 23
+
+// Calling function with destructured object parsing to the function
+restaurant.orderDelivery({
+	time: '23:28',
+	address: '123 Street 01',
+	mainIndex: 2,
+	starterIndex: 1
+});
+restaurant.orderDelivery({
+	address: '456 Street 02'
+});
+
+/* Lec105. The Spread Operator (...) */
+// Unpacking all elements of an array at once
+const arr1 = [ 4, 5, 6, 7 ];
+const badNewArr = [ 1, 2, 3, arr1[0], arr1[1], arr1[2], arr1[3] ]; //adding 3 new elements to arr1
+console.log(badNewArr); //[ 1, 2, 3, 4, 5, 6, 7 ]
+
+// Do faster using spread operator (...)
+const newArr = [ 1, 2, 3, ...arr1 ]; //...arr1: all elements in arr1 separated by ','
+console.log(newArr); //[ 1, 2, 3, 4, 5, 6, 7 ]
+
+// NOTE:
+const newArrNested = [ 1, 2, 3, arr1 ]; //add the WHOLE array [ 4, 5, 6, 7 ] to [ 1, 2, 3]
+console.log(newArrNested); //[ 1, 2, 3, [4, 5, 6, 7 ]]
+
+console.log(arr1); //[4, 5, 6, 7]
+console.log(...arr1); //4 5 6 7
+console.log(4, 5, 6, 7); //4 5 6 7
+
+// (...) is useful when parsing arguments into functions or any time we want variables separated by ','
+const newMenu = [ ...restaurant.mainMenu, 'Gnocci' ]; //add new dish 'Gnocci' to the main menu
+console.log('newMenu:', newMenu);
+
+/*const buyFruits = function(fruit1, fruit2, fruit3) {
+	console.log(`You have bought: ${fruit1}, ${fruit2}, ${fruit3}`);
+};
+const fruits = [ prompt('Fruit #1:'), prompt('Fruit #2:'), prompt('Fruit #3:') ];
+console.log(fruits);
+
+//buyFruits(fruits[0], fruits[1], fruits[2]);
+buyFruits(...fruits); //parsing arguments into function using (...) operator
+*/
+
+// Duplicate array
+const newMenu_Copy = [ ...newMenu ];
+console.log('newMenu_Copy:', newMenu_Copy);
+
+// Merge 2 arrays
+const newMenu_Merged = [ ...restaurant.starterMenu, ...restaurant.mainMenu ];
+console.log('newMenu_Merged:', newMenu_Merged);
+
+// NOTE: (...) can be used with iterable types: arrays, strings, maps, sets (even objects in ES2018)
+//// (...) with strings
+const str = 'Kathy';
+const letters = [ ...str, ...'Pham' ];
+console.log(letters); //["K", "a", "t", "h", "y", "P", "h", "a", "m"]
+console.log(...str); //K a t h y
+
+//// (...) with objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log('newRestaurant:', newRestaurant);
+
+//// Duplicate objects
+const restaurantCopy = { ...newRestaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name); //still remains original values: 'Classico Italiano'
+
+/* Lec106. Rest Pattern and Parameters */
+// Same syntax as Spread (...) operator, but do the opposite
+// => Collect multiple elements to make an array
+
+//// Spread (...) operator is on the RIGHT of '='
+const arr2 = [ 1, 2, ...[ 3, 4 ] ]; //spread -> unpacking [3, 4] to 3, 4
+console.log(arr2);
+
+//// Rest (...) operator is on the LEFT of '='
+//// NOTE: (...) must be the last element
+const [ p, q, ...others ] = [ 1, 2, 3, 4, 5 ];
+console.log(p, q, others); //packing [3, 4, 5] to an array 'others'
+
+const [ pizza, risotto, ...otherFood ] = [ ...restaurant.mainMenu, ...restaurant.starterMenu ];
+console.log(...restaurant.mainMenu, ...restaurant.starterMenu); //Pizza Pasta Risotto Focaccia Bruschetta Garlic Bread Caprese Salad
+console.log(otherFood); //Risotto Focaccia Bruschetta Garlic Bread Caprese Salad
