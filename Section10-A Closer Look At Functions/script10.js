de276eefb3cf8ac293cca33ec811b98b43faf2e3
thipham0502 <thipham0502@gmail.com>
2021-01-03 23:07:56 +0700

    Jan03_2307

--- Section10-A Closer Look At Functions/script10.js
+++ Section10-A Closer Look At Functions/script10.js
@@ -1,5 +1,7 @@
 'use script';
 
+/* Lec126. Default Parameters */
+console.log('----------Lec126----------');
 const bookings = [];
 const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers) {
 	// Setting default values (ES5)
@@ -19,3 +21,24 @@ createBooking('LH123', 2);
 createBooking('LH123', 2, 800);
 // createBooking('LH123', , 800); //error
 createBooking('LH123', undefined, 800);
+
+/* Lec127. How parsing arguments works: Value vs. Reference */
+console.log('----------Lec127----------');
+const flight = 'LH234';
+const kathy = {
+	name     : 'Kathy Pham',
+	passport : 123456789
+};
+const checkin = function(flightNum, passenger) {
+	flightNum = 'LH999';
+	passenger.name = 'Ms.' + passenger.name;
+
+	if (passenger.passport === 123456789) {
+		alert('Check in');
+	} else {
+		alert('Wrong passport!');
+	}
+};
+checkin(flight, kathy);
+console.log(flight);
+console.log(kathy);
